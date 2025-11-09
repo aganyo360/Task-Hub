26697c7a38da95c90b9d0cd76c88b0ca686ef951
aganyo360 <wilsonaganyo@gmail.com>
2025-11-09 14:14:36 +0300

    CRUD task operations complete'

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -3,4 +3,11 @@ from . import  views
 
 urlpatterns =[
     path('', views.index, name="index"),
+    path('task_list', views.task_list, name='task_list'),
+    path('task_details<int:id>', views.task_details, name='task_details'),
+    path('add_task', views.add_task, name= "add_task"),
+    path('complete/<int:id>', views.complete_task, name= "complete_task"),
+    path('delete/<int:id>', views.delete_task, name= "delete_task"),
+
+
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import ToDoTask
 
 # Create your views here.
 def index(req):
-    return render(req, "todoapp/index")
+    return render(req, "todoapp/index.html")
+
+def task_list(req):
+    task = ToDoTask.objects.all()
+    task_count = len(task.values())
+    return render(req,"todoapp/list_task.html", {'task':task, 'task_count':task_count})
+
+def add_task(req):
+    if req.method =="POST":
+        title = req.POST['title']
+        description = req.POST['description']
+        task = ToDoTask.objects.create(title=title, description=description)
+        task.save()
+        return redirect('task_list')
+    return render(req, "todoapp/add_task.html")
+
+    
+def complete_task(req, id):
+    task = ToDoTask.objects.get(id=id)
+    task.completed = True
+    task.save()
+    return redirect("task_details")
+    
+
+
+def task_details(req, id):
+    task = ToDoTask.objects.get(id=id)
+    return render(req, "todoapp/task_details.html", {"task":task})
+
+def delete_task(req, id):
+    task = ToDoTask.objects.get(id=id)
+    task.delete()
+    return redirect('task_list')
