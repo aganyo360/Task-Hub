d5276dbe8fd38bb394c7195f17782241fb355163
aganyo360 <wilsonaganyo@gmail.com>
2025-11-09 21:26:51 +0300

    add complete and update view

--- todoapp/views.py
+++ todoapp/views.py
@@ -3,7 +3,8 @@ from .models import ToDoTask
 
 # Create your views here.
 def index(req):
-    return render(req, "todoapp/index.html")
+    task= ToDoTask.objects.all()
+    return render(req, "todoapp/index.html", {'task':task})
 
 def task_list(req):
     task = ToDoTask.objects.all()
@@ -24,15 +25,24 @@ def complete_task(req, id):
     task = ToDoTask.objects.get(id=id)
     task.completed = True
     task.save()
-    return redirect("task_details")
+    return redirect("task_details", id=task.id)
     
-
 
 def task_details(req, id):
     task = ToDoTask.objects.get(id=id)
+    print(req)
     return render(req, "todoapp/task_details.html", {"task":task})
 
 def delete_task(req, id):
     task = ToDoTask.objects.get(id=id)
     task.delete()
     return redirect('task_list')
+
+def update_task(req, id):
+    task = ToDoTask.objects.get(id=id)
+    if req.method =='POST':
+        task.title= req.POST.get('title')
+        task.description= req.POST.get('description')
+        task.save()
+        return redirect("task_details",id= task.id)
+    return render(req, "todoapp/update_task.html", {'task': task})
